9b303154c9844dbd8f06aae7ec80090d84a38b1a
Ian S. McBride <ian.s.mcb@gmail.com>
2023-01-31 18:15:39 -0500

    Configure Vite with resolve.alias (aka import alias)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import { URL } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // See below for explanation about the resolve.alias config setting
+  // https://vitejs.dev/config/shared-options.html#resolve-alias
+  resolve: {
+    alias: {
+      // See below for explanation about forming path names to files
+      // https://stackoverflow.com/a/66651120
+      '@': new URL('./src', import.meta.url).pathname,
+    },
+  },
 })
