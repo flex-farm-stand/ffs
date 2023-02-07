a8511beb43f32d6b537caf3759663b35da1f028f
Ian S. McBride <ian.s.mcb@gmail.com>
2023-02-07 16:55:24 -0500

    Enable svgr plugin in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { URL } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   // See below for explanation about the resolve.alias config setting
   // https://vitejs.dev/config/shared-options.html#resolve-alias
   resolve: {
