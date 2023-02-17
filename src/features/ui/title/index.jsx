7cb62f978f3d3d2bc57595476f4cf02402e8da6c
Ian S. McBride <ian.s.mcb@gmail.com>
2023-02-17 14:00:59 -0500

    Add style to ui/button, ui/title

--- src/features/ui/title/index.jsx
+++ src/features/ui/title/index.jsx
@@ -1,3 +1,11 @@
-export function Title({ text = '' }) {
-  return <div>{text}</div>
+import styled from 'styled-components'
+
+function VanillaTitle({ className, text = '' }) {
+  return <div className={className}>{text}</div>
 }
+
+export const Title = styled(VanillaTitle).attrs({
+  className: 'title',
+})`
+  font-size: 1.5rem;
+`
