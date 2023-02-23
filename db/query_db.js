cf085acb350858c59ae604c3702dd48a5b0a304d
Ian S. McBride <ian.s.mcb@gmail.com>
2023-02-23 12:51:41 -0500

    Rename, expand js script in ffs/db

--- db/query_db.js
+++ db/query_db.js
@@ -4,11 +4,26 @@ import { createClient } from '@supabase/supabase-js'
 
 dotenv.config({ path: './.env.local' })
 const supabaseUrl = env.VITE_SUPABASE_URL
-// const supabaseAnonKey = env.VITE_SUPABASE_ANON_KEY
+const supabaseAnonKey = env.VITE_SUPABASE_ANON_KEY
 const supabaseServiceKey = env.SUPABASE_SERVICE_KEY
 
+async function getAllProfiles() {
+  const supabase = createClient(supabaseUrl, supabaseAnonKey)
+  const data = await supabase
+    .from('profiles')
+    .select()
+    .eq('id', '46dab3ce-4a1e-44d2-95c7-45af71314b44')
+  console.log(data)
+}
+
+async function getSession() {
+  const supabase = createClient(supabaseUrl, supabaseAnonKey)
+  const data = await supabase.auth.getSession()
+  console.log(data)
+}
+
 // Demonstrates how to use the Auth Admin, which requires the service key
-async function listAllUsers() {
+async function getAllUsers() {
   const supabase = createClient(supabaseUrl, supabaseServiceKey, {
     auth: {
       autoRefreshToken: false,
@@ -21,4 +36,6 @@ async function listAllUsers() {
   console.log(users)
 }
 
-listAllUsers()
+getAllUsers()
+// getSession()
+// getAllProfiles()
