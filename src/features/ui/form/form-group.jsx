30e58c55a63ceceafc738370533c35b063ff81b7
Ian S. McBride <ian.s.mcb@gmail.com>
2023-02-26 01:14:32 -0500

    Add ref to form/form-group for input focusing

--- src/features/ui/form/form-group.jsx
+++ src/features/ui/form/form-group.jsx
@@ -1,27 +1,34 @@
+import { forwardRef } from 'react'
+
 import styled from 'styled-components'
 
-function VanillaFormGroup({
-  autoFocus = false,
-  className,
-  label = '',
-  onChange,
-  placeholder = '',
-  type = 'text',
-  value,
-}) {
-  return (
+const VanillaFormGroup = forwardRef(
+  (
+    {
+      autoFocus = false,
+      className,
+      label = '',
+      onChange,
+      placeholder = '',
+      type = 'text',
+      value,
+    },
+    ref
+  ) => (
     <div className={className}>
       {label && <label>{label}</label>}
       <input
         autoFocus={autoFocus}
         onChange={onChange}
         placeholder={placeholder}
+        ref={ref}
         type={type}
         value={value}
       />
     </div>
   )
-}
+)
+VanillaFormGroup.displayName = 'VanillaFormGroup'
 
 export const FormGroup = styled(VanillaFormGroup).attrs({
   className: 'form-group',
