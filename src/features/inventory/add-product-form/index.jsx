flex-farm-stand/ffs

commit 989c13b9ea3bc1a91047308fa07d662bb458cc3a

Refactor page body margin
- Move auto margin element from features/{inventory,users} -> ui/container
- Use element in pages/{inventory,profile}

src/features/inventory/add-product-form/index.jsx
@@ -10,13 +10,6 @@ import {
   Title,
 } from '@/features/ui'
 
-const Container = styled.div`
-  display: flex;
-  gap: 0.5rem;
-  margin: 2rem auto;
-  max-width: 600px;
-`
-
 const StyledForm = styled(Form)`
   & {
     max-width: 300px;
@@ -88,7 +81,7 @@ export function AddProductForm({
   uploading,
 }) {
   return (
-    <Container>
+    <div>
       <StyledForm onSubmit={onInsert}>
         <FlexBetween>
           <Title text="Add product" />
@@ -134,6 +127,6 @@ export function AddProductForm({
         <FormFeedback feedback={feedback} />
       </StyledForm>
       <ImagePreview>{imageUrl && <img src={imageUrl} />}</ImagePreview>
-    </Container>
+    </div>
   )
 }

src/features/inventory/inventory-list/index.jsx
@@ -4,14 +4,6 @@ import { Title } from '@/features/ui'
 
 const emptyInventoryText = 'There are no items in your inventory at the moment.'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 2rem auto;
-  max-width: 600px;
-  overflow-x: auto;
-`
-
 function Greeting({ count }) {
   return (
     <p>
@@ -79,7 +71,7 @@ export function InventoryList({ attributes, handleCheckboxChange, inventory }) {
   const thead = attributes.map(ths)
 
   return (
-    <Container>
+    <div>
       <Title text="Inventory" />
       <Greeting count={!inventory ? 0 : inventory.length} />
       <Table>
@@ -88,6 +80,6 @@ export function InventoryList({ attributes, handleCheckboxChange, inventory }) {
         </thead>
         <tbody>{tbody}</tbody>
       </Table>
-    </Container>
+    </div>
   )
 }

src/features/ui/container/index.jsx
@@ -1,5 +1,13 @@
 import styled from 'styled-components'
 
+export const CenterAndLimitWidth = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin: 2rem auto;
+  max-width: 600px;
+`
+
 export const FlexBetweenAndReorder = styled.div.attrs({
   className: 'flex-between-and-reorder',
 })`

src/features/users/account-form/index.jsx
@@ -11,9 +11,7 @@ import {
 
 const StyledForm = styled(Form)`
   & {
-    margin: 0 auto;
     max-width: 400px;
-    padding: 2rem;
   }
   /*
     Math for input width

src/features/users/profile-form/index.jsx
@@ -11,9 +11,7 @@ import {
 
 const StyledForm = styled(Form)`
   & {
-    margin: 0 auto;
     max-width: 400px;
-    padding: 2rem;
   }
   /*
     Math for input width

src/features/ui/index.js
@@ -1,5 +1,5 @@
 export { Button } from './button'
-export { FlexBetween, FlexBetweenAndReorder } from './container'
+export { CenterAndLimitWidth, FlexBetween, FlexBetweenAndReorder } from './container'
 export { ButtonLabelPair, Form, FormFeedback, InputLabelPair } from './form'
 export { Logo, LogoLink } from './logo'
 export { Menu } from './menu'

src/pages/inventory.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 import { AddProductForm, InventoryList } from '@/features/inventory'
 import { useSupabaseClient } from '@/features/supabase'
 import { useAuth } from '@/features/users'
+import { CenterAndLimitWidth } from '@/features/ui'
 
 const initialFeedback = { status: '', message: '' }
 const initialName = ''
@@ -133,7 +134,7 @@ export function Inventory() {
 
   return (
     !loading && (
-      <>
+      <CenterAndLimitWidth>
         <AddProductForm
           editing={editing}
           feedback={feedback}
@@ -153,7 +154,7 @@ export function Inventory() {
           handleCheckboxChange={handleCheckboxChange}
           inventory={inventory}
         />
-      </>
+      </CenterAndLimitWidth>
     )
   )
 }

src/pages/profile.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 
-import { AccountForm, ProfileForm, useAuth } from '@/features/users'
 import { useSupabaseClient } from '@/features/supabase'
+import { CenterAndLimitWidth } from '@/features/ui'
+import { AccountForm, ProfileForm, useAuth } from '@/features/users'
 
 const successEmailChange =
   'Check your email - both the old and new email addresses. Two email ' +
@@ -79,7 +80,7 @@ export function Profile() {
   }
 
   return (
-    <>
+    <CenterAndLimitWidth>
       <AccountForm
         editing={editingAccount}
         email={email}
@@ -98,6 +99,6 @@ export function Profile() {
         onSubmit={onSubmitProfile}
         reset={resetProfile}
       />
-    </>
+    </CenterAndLimitWidth>
   )
 }

commit 8a8f8b22bd365889cd522bb7c746148b4e0d098c

Fix style regression from 989c13b
Regression consisted of the styled form and the image preview elements in
inventory/add-product-form being stacked vertically instead of horizontally.

src/features/inventory/add-product-form/index.jsx
@@ -10,6 +10,11 @@ import {
   Title,
 } from '@/features/ui'
 
+const Container = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
+
 const StyledForm = styled(Form)`
   & {
     max-width: 300px;
@@ -81,7 +86,7 @@ export function AddProductForm({
   uploading,
 }) {
   return (
-    <div>
+    <Container>
       <StyledForm onSubmit={onInsert}>
         <FlexBetween>
           <Title text="Add product" />
@@ -127,6 +132,6 @@ export function AddProductForm({
         <FormFeedback feedback={feedback} />
       </StyledForm>
       <ImagePreview>{imageUrl && <img src={imageUrl} />}</ImagePreview>
-    </div>
+    </Container>
   )
 }