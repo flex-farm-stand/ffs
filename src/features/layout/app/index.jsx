9177753f0ebaf268af89a186430011b2107b58f0
Ian S. McBride <ian.s.mcb@gmail.com>
2023-03-10 14:38:40 -0500

    Use React Query in layout/app

--- src/features/layout/app/index.jsx
+++ src/features/layout/app/index.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { Nav } from '@/features/layout'
 import { SupabaseProvider } from '@/features/supabase'
@@ -8,21 +10,32 @@ import { AuthProvider } from '@/features/users'
 import { GlobalStyle } from './global-style'
 import { themes } from './themes'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 export function App() {
   const [theme, effectiveTheme, setTheme] = useDarkMode()
 
   return (
     <SupabaseProvider>
       <AuthProvider>
-        <DarkModeContext.Provider value={{ theme, effectiveTheme, setTheme }}>
-          <ThemeProvider theme={themes[effectiveTheme]}>
-            <div className="app">
-              <GlobalStyle />
-              <Nav />
-              <Outlet />
-            </div>
-          </ThemeProvider>
-        </DarkModeContext.Provider>
+        <QueryClientProvider client={queryClient}>
+          <DarkModeContext.Provider value={{ theme, effectiveTheme, setTheme }}>
+            <ThemeProvider theme={themes[effectiveTheme]}>
+              <div className="app">
+                <GlobalStyle />
+                <Nav />
+                <Outlet />
+              </div>
+            </ThemeProvider>
+          </DarkModeContext.Provider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </AuthProvider>
     </SupabaseProvider>
   )
