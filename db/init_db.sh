04fdba07004bf55eb30b17f2bd718a5982e0125f
Ian S. McBride <ian.s.mcb@gmail.com>
2023-04-16 14:54:54 -0400

    Improve reusability of init_db.sh
    Put more connection details in variables to make it simpler to tailor the script
    to developer's env.

--- db/init_db.sh
+++ db/init_db.sh
@@ -3,14 +3,16 @@
 #  - the credentials are the defaults from self-hosted Supabase via Docker
 #  - dbname, host, port, username values need to changed to real credentials
 
-# Step-0 - Store db password
+# Step-0 - Store db connection details
+export PGHOST=localhost
 export PGPASSWORD=your-super-secret-and-long-postgres-password
+export PGPORT=5423
 
 # Step-1 - (optional) Wipe db - already included in `create_db.sql` script
-psql --dbname="postgres" --host=localhost --port=5432 --username=postgres -f ./db/wipe_db.sql
+psql --dbname="postgres" --host=$PGHOST --port=$PGHOST --username=postgres -f ./db/wipe_db.sql
 
 # Step-2 - Create db
-psql --dbname="postgres" --host=localhost --port=5432 --username=postgres -f ./db/create_db.sql
+psql --dbname="postgres" --host=$PGHOST --port=$PGHOST --username=postgres -f ./db/create_db.sql
 
 # Step-3 - Seed db
-psql --dbname="postgres" --host=localhost --port=5432 --username=postgres -f ./db/seed_db.sql
+psql --dbname="postgres" --host=$PGHOST --port=$PGHOST --username=postgres -f ./db/seed_db.sql
