ef660971dd47b546612d47fd10a3415caa82b1f5
Ian S. McBride <ian@iansmcb.com>
2024-02-24 10:34:16 -0500

    Adjust vars in init_db.sh
    - Make vars for all params
    - Quote all params

--- db/init_db.sh
+++ db/init_db.sh
@@ -4,15 +4,17 @@
 #  - dbname, host, port, username values need to changed to real credentials
 
 # Step-0 - Store db connection details
+export PGDATABASE=postgres
 export PGHOST=localhost
 export PGPASSWORD=your-super-secret-and-long-postgres-password
 export PGPORT=5432
+export PGUSER=postgres
 
 # Step-1 - (optional) Wipe db - already included in `create_db.sql` script
-psql --dbname="postgres" --host=$PGHOST --port=$PGPORT --username=postgres -f ./db/wipe_db.sql
+psql --dbname="$PGDATABASE" --host="$PGHOST" --port="$PGPORT" --username="$PGUSER" -f ./db/wipe_db.sql
 
 # Step-2 - Create db
-psql --dbname="postgres" --host=$PGHOST --port=$PGPORT --username=postgres -f ./db/create_db.sql
+psql --dbname="$PGDATABASE" --host="$PGHOST" --port="$PGPORT" --username="$PGUSER" -f ./db/create_db.sql
 
 # Step-3 - Seed db
-psql --dbname="postgres" --host=$PGHOST --port=$PGPORT --username=postgres -f ./db/seed_db.sql
+psql --dbname="$PGDATABASE" --host="$PGHOST" --port="$PGPORT" --username="$PGUSER" -f ./db/seed_db.sql
